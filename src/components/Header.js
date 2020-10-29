bf8fa983620f78fbc8c9ca03a6e2a7bfa68ae6c1
rorschach777 <msweitzer1987@gmail.com>
2020-10-29 09:02:33 -0400

    header updated with logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { PageHeader, Button, Descriptions } from 'antd';
 
+import Logo from './Logo';
 import Menu from './Menu';
 import DrawerMenu from './DrawerMenu';
 class Header extends React.Component {
@@ -28,23 +28,12 @@ class Header extends React.Component {
     };
     render() {
         return (
-            <PageHeader
-            className="site-page-header header"
-            // onBack={() => {
-            //     router.push("/")
-            // }}
-            title="Title"
-            subTitle="This is a subtitle"
-            extra={[
-                <Menu key="Header-Component-1"/>,
-                <DrawerMenu key="Header-Component-2" 
-                visible={this.state.visible} 
-                placement={this.state.placement}
-                onClose={this.onClose}
-                />
-            ]}
-            >
-           </PageHeader>
+           <div className="Header">
+             <div className="Header__inner">
+              <Logo/>
+              <Menu/>
+             </div>
+           </div>
         );
     }
 }
---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = (props) => {
+    return (
+        <div className="Logo">
+            <img src="http://localhost:8888/laboski-backend/dist/wp-content/themes/55OVER34/images/logo.svg"/>
+        </div>
+    );
+};
+
+export default Logo;
